71f0a68292cbe3e6ce5512746b498f3706a16b2b
Will Jackson <wrejcodes@gmail.com>
2017-11-03 19:06:22 -0500

    included prims main to test it and make sure it compiles

--- prims.cpp
+++ prims.cpp
@@ -281,3 +281,29 @@ void PrimMST(struct Graph* graph)
     // print edges of MST
     printArr(parent, V);
 }
+
+
+int main()
+{
+    // Let us create the graph given in above fugure
+    int V = 9;
+    struct Graph* graph = createGraph(V);
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 7, 8);
+    addEdge(graph, 1, 2, 8);
+    addEdge(graph, 1, 7, 11);
+    addEdge(graph, 2, 3, 7);
+    addEdge(graph, 2, 8, 2);
+    addEdge(graph, 2, 5, 4);
+    addEdge(graph, 3, 4, 9);
+    addEdge(graph, 3, 5, 14);
+    addEdge(graph, 4, 5, 10);
+    addEdge(graph, 5, 6, 2);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 6, 8, 6);
+    addEdge(graph, 7, 8, 7);
+ 
+    PrimMST(graph);
+ 
+    return 0;
+}
